d96a328eebacafe2eb460635cfa1b383c35e30d9
vinicius Spinellis <vspinellis@hotmail.com>
2022-09-18 12:54:51 -0300

    garantir que o emailvalidator tenha sido chamado com o email correto

--- src/utils/validators/email-validator.spec.js
+++ src/utils/validators/email-validator.spec.js
@@ -8,13 +8,18 @@ describe('Email Validator', () => {
   })
   test('deve retornar true se validator retornar true', () => {
     sut.isValid.mockImplementationOnce(() => true)
-    const isEmailValid = sut.isValid('valid_email.com')
+    const isEmailValid = sut.isValid('valid_email')
     expect(isEmailValid).toBe(true)
   })
 
   test('deve retornar false se validator retornar false', () => {
     sut.isValid.mockImplementationOnce(() => false)
-    const isEmailValid = sut.isValid('invalid_email.com')
+    const isEmailValid = sut.isValid('invalid_email')
     expect(isEmailValid).toBe(false)
+  })
+
+  test('deve garantir que o emailValidator será chamado com o email correto', () => {
+    sut.isValid('any_email')
+    expect(sut.isValid).toHaveBeenCalledWith('any_email')
   })
 })
