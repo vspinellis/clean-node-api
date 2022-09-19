cf1a78c137f03a4c44978c30aba28ea94e0cc63b
vinicius Spinellis <vspinellis@hotmail.com>
2022-09-19 16:29:27 -0300

    loadUserRepository

--- src/infra/repositories/load-user-by-email-repository.js
+++ src/infra/repositories/load-user-by-email-repository.js
@@ -1,6 +1,19 @@
+const MongoHelper = require('../helpers/mongo-helper')
+
 class LoadUserByEmailRepository {
   async load (email) {
-    return null
+    if (!email) {
+      throw new Error()
+    }
+    const userModel = await MongoHelper.getCollection('users')
+    const user = await userModel.findOne({
+      email
+    }, {
+      projection: {
+        password: 1
+      }
+    })
+    return user
   }
 }
 
