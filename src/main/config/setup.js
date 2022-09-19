c3d53350f575afa1afa26cde2e2962fd7f0744b2
vinicius Spinellis <vspinellis@hotmail.com>
2022-09-19 17:06:46 -0300

    mover cors para arquivo separado

--- src/main/config/setup.js
+++ src/main/config/setup.js
@@ -1,3 +1,6 @@
+const cors = require('../middlewares/cors')
+
 module.exports = app => {
   app.disable('x-powered-by')
+  app.use(cors)
 }
--- src/main/config/setup.test.js
+++ src/main/config/setup.test.js
@@ -9,4 +9,11 @@ describe('App Setup', () => {
     const res = await request(app).get('/test_x_powered_by')
     expect(res.headers['x-powered-by']).toBeUndefined()
   })
+
+  test('deve enable cors', async () => {
+    const res = await request(app).get('/test_cors')
+    expect(res.headers['access-control-allow-origin']).toBe('*')
+    expect(res.headers['access-control-allow-methods']).toBe('*')
+    expect(res.headers['access-control-allow-headers']).toBe('*')
+  })
 })
